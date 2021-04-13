f469c05d76030db8c6a2135f834086110e032c36
weijieni <wni@deakin.edu.au>
2021-04-13 20:21:09 +0800

    some updates on nav bar

--- public/env.js
+++ public/env.js
@@ -7,23 +7,10 @@ socket.on('number', (msg) => {
 })
 
 console.log('test')
-// $(document).ready(function(){
-//   console.log('Ready')
-  
-//   //test get call
-//   $.get('/test?user_name="Fantastic User"',(result)=>{
-//     console.log(result)
-//   })
-// })
 
-// $(document).ready(function(){
-//   $('.button-collapse').click(() => {
-//     console.log($('.side-nav'))
-//     $('#slide-out').show(300);
-//   })
-// });
 
 $(function() {
+  //handle nav-list click task
   $('#nav_dev').click(function() {
     var dev_item = $('#dev_item')
     if (dev_item.css("display") == "none") {
@@ -32,19 +19,28 @@ $(function() {
       dev_item.hide(300)
     }
   })
+
+  //handle nav-bar show and hide
+  $('.nav_icon').click(function() {
+    var nav_bar = $('.nav')
+    if (nav_bar.css("display") == "none") {
+      nav_bar.show(200)
+    } else {
+      nav_bar.hide(200)
+    }
+  })
 })
 
-function handelTab(id, bid) {
+//handle nav-button click task - tab switch
+function handleTab(id, bid) {
   // tab switch
   $('.pages').fadeOut(300)
   $(id).fadeIn(300)
 
   // button color
   // reset button color and font color
-  $('.navbutton').css("background-color", "#fff");
-  $('.navbutton').css("color", "#000");  
+  $('.navbutton').removeClass('btn_selected')
   //change selected button color and font color
-  $(bid).css("background-color", "#1a27ba");
-  $(bid).css("color", "#fff");
+  $(bid).addClass('btn_selected')
 }
 
