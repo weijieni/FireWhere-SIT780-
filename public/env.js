0b98adb5174be18a169cd1ba0cd8b8137484868a
weijieni <wni@deakin.edu.au>
2021-04-16 21:23:49 +0800

    updated research card add method

--- public/env.js
+++ public/env.js
@@ -245,6 +245,50 @@ let data_data = [
     warning: "Red",
   }
 ]
+let card_data = [
+  {
+    id: 0,
+    header: "This is the header",
+    imageUrl: "assets/1.jpg",
+    content: "This is the content",
+    link: "#"
+  },
+  {
+    id: 0,
+    header: "This is the header",
+    imageUrl: "assets/1.jpg",
+    content: "This is the content",
+    link: "#"
+  },
+  {
+    id: 1,
+    header: "This is the header",
+    imageUrl: "assets/1.jpg",
+    content: "This is the content",
+    link: "#"
+  },
+  {
+    id: 2,
+    header: "This is the header",
+    imageUrl: "assets/1.jpg",
+    content: "This is the content",
+    link: "#"
+  },
+  {
+    id: 3,
+    header: "This is the header",
+    imageUrl: "assets/1.jpg",
+    content: "This is the content",
+    link: "#"
+  },
+  {
+    id: 4,
+    header: "This is the header",
+    imageUrl: "assets/1.jpg",
+    content: "This is the content",
+    link: "#"
+  },
+]
 
 socket.on('number', (msg) => {
 })
@@ -293,6 +337,7 @@ $(function() {
   //   ifUrgent.css("color", "green")
   // }
 
+  //create tables
   $('#warning_table').bootstrapTable({
     data: warning_data,
     classes: 'table'
@@ -312,6 +357,9 @@ $(function() {
     data: data_data,
     classes: 'table'
   })
+
+  //add cards
+  addCards(card_data)
 })
 
 //handle nav-button click task - tab switch
@@ -332,6 +380,32 @@ function operateFormatter(value, row, index) {
   return `
   <button class="" data-bs-toggle="modal" data-bs-target="#staticBackdrop">${warning_data[index].warning_message}</button>
   `
+}
+
+//create research cards html
+function addCards(dataObj) {
+  let div = ""
+  for (let i=0; i<dataObj.length;i++) {
+    div += `
+    <div class="research_card">
+      <h3 class="card_header">${dataObj[i].header}</h3>
+        <div class="card_main">
+          <div class="card_image">
+            <img src="${dataObj[i].imageUrl}">
+          </div>
+          <div class="card_stacked">
+            <div class="card_content">
+              <p>${dataObj[i].content}</p>
+            </div>
+            <div class="card_action">
+              <a class="link" href="${dataObj[i].link}">This is the link</a>
+            </div>
+          </div>
+        </div>
+    </div>
+    `
+  }
+  $('#research_cards').html(div)
 }
 
 // //initial table data - warning tab
